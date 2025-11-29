cf815ff0ee97b1b37beecfaf1762b9d0487b07c8
dajaykumar <d.ajaykumar@amadeus.com>
2025-11-29 11:27:51 +0530

    v0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="argoflow",
-    version="0.2.0",
+    version="0.2.1",
     author="D Ajay Kumar",
     description="A Python library for building Argo Workflows programmatically",
     long_description=long_description,
